7795691e0eaabc82c55cc52dd70239ed79fca2b0
Ivan Ustyantsev <vangold915@gmail.com>
2023-11-28 16:50:06 +0700

    Validate index function created

--- homework3/tic_tac_toe.py
+++ homework3/tic_tac_toe.py
@@ -21,17 +21,17 @@ board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def draw_board():
     """Функция отрисовки игрового поля"""
-    # print('_' + ('_' * 4) * board_side)
-    # for i in range(board_side):
-    #     print('|' + (' ' * 3 + '|')*3)
-    #     print('|', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|') 
-    #     print('|' + ('_' * 3 + '|') * board_side)
-    
-    print(('_' * 4) * board_side)
+    print('_' + ('_' * 4) * board_side)
     for i in range(board_side):
-        print((' ' * 3 + '|')*3)
-        print('', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|') 
-        print(('_' * 3 + '|') * board_side)
+        print('|' + (' ' * 3 + '|')*3)
+        print('|', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|') 
+        print('|' + ('_' * 3 + '|') * board_side)
+
+    # print(('_' * 4) * board_side)
+    # for i in range(board_side):
+    #     print((' ' * 3 + '|')*3)
+    #     print('', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|') 
+    #     print(('_' * 3 + '|') * board_side)
 
 def game_step():
     """Функция для выполнения шага игры"""
@@ -39,10 +39,38 @@ def game_step():
 
 def check_win():
     """Функция отслеживания победы"""
-    pass
+    return False
+
+def validate_index(index, player):
+    not_ready = True
+    while (not_ready):
+        if (not index.isdigit()):
+            print('Вы ввели не число, попробуйте еще раз!')
+            index = input(f'Ходит игрок {player}. Введите номер ячейки (0 - выход из игры): ')
+        if (int(index) == 0):
+            break
+        elif (int(index) < 1 or int(index) > len(board)):
+            print('Вы ввели число, выходящее за пределы игрового поля!')
+            print('Попробуйте ввести значение снова!')
+            index = (input(f'Ходит игрок {player}. Введите номер ячейки (0 - выход из игры): '))
+        else:
+            not_ready = False
+    return index
 
 def start_game():
+    # Текущий игрок
+    current_player = 'X'
+    # Текущий шаг
+    current_step = 1
     draw_board()
 
+    while (current_step < len(board) + 1) and (check_win() == False):
+        index = input(f'Ходит игрок {current_player}. Введите номер ячейки (0 - выход из игры): ')
+        index = validate_index(index, current_player)
+        if (int(index) == 0):
+                print("Игра завершена!")
+                break
+        current_step += 1
+        
 print('Добро пожаловать в игру "Крестики-нолики"')
 start_game()
